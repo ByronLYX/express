916c71f5ecbf08e57ec0880793be07fb294eaefd
Byron <1611072395@qq.com>
2019-04-30 18:19:15 +0800

    second

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require("express");
+var AV = require('leanengine');
+
+AV.init({
+  appId: process.env.LEANCLOUD_APP_ID || 'YJq8EmK9dBLLb8rNbHgVKThH-gzGzoHsz',
+  appKey: process.env.LEANCLOUD_APP_KEY || 'kO0dXTuU5HdmKAk2As18k9EC',
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || '9vgptL94hYkJEzIYUPbmqTQT'
+});
 var app = express();
+
+app.use(AV.express());
+
 app.get("/", function(req, res){
     res.end("123");
 })
-app.listen(3000);
+
+app.listen(process.env.LEANCLOUD_APP_PORT);
